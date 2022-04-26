3895da7d85e82e2b25c368f2c0e10eb5944b1280
Ирина Ефимова <efimovair@gmail.com>
2022-04-26 23:45:36 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(['auth', 'verified']);
 
 Route::resource('applications', App\Http\Controllers\ClientApplication\ClientApplicationController::class)->middleware(['auth', 'verified']);
 
