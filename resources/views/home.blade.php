b530738d9c88ca36370e275d8899336c8837ae57
Ирина Ефимова <efimovair@gmail.com>
2022-04-26 23:41:57 +0300

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header"><ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('applications.index')}}">Список заявок</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('applications.create')}}">Новая заявка</a>
+                        </li>
+                    </ul>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
